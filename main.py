25b319fbe5b9124ba97baf4c46c417d7870eb14d
krzysztof zinger <krzysztof.zinger@gmail.com>
2020-02-11 23:58:16 +0100

    stable. Menu to be done

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pygame, sys
 from settings import *
 from pygame.locals import *
-from meteors import meteors, add_meteor_row
+from meteors import meteors, add_meteor_row, exploded_meteors_list
 from spacecraft import craft, lunched_shots
 from playerstats import player_stats
 
-print('hello')
+print('Space craft game initialized.')
 
 pygame.init()
 # pygame.font.init()
@@ -14,11 +14,19 @@ fpsClock = pygame.time.Clock()
 #Caption
 pygame.display.set_caption('pygame test')
 
+#variables
+meteor_vector = [0,1]
+old_player_points = player_stats.level
+
 # draw first line of meteors
 add_meteor_row()
 
 #Main Loop
 while True:
+
+    if player_stats.health <= 0:
+        break
+
     DISPLAYSURF.fill(colors['black'])
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -55,12 +63,17 @@ while True:
         print(meteors.meteors_list[-1].y)
 
     #draw geometries
+    exploded_meteors_list.draw() # maybe change to traverse later on ??
     meteors.traverse(call_function='draw',)
     lunched_shots.draw()
     craft.draw()
 
     # update position of geometries
-    meteors.traverse(call_function='move', vector=[0,.5])
+    if old_player_points + 15 < player_stats.points:
+        old_player_points = player_stats.points
+        player_stats.level += 1
+        meteor_vector = [meteor_vector[0],meteor_vector[1]+.5]
+    meteors.traverse(call_function='move', vector=meteor_vector)
 
     # handle collisions
     meteors.traverse(call_function='colision', colision_items=lunched_shots)
--- meteors.py
+++ meteors.py
@@ -3,12 +3,17 @@
 Meteors are stored as a linked list with root and last node.
 Nodes have two pointers. One to next and one to previous node.
 '''
-
+# python modules
 import pygame, sys
-from settings import *
-from colisions import detect_colision
-from playerstats import player_stats
+from random import randint, choice
+import time
+
+#local imports
+from colisions import detect_colision, left_game_area
 from base_obj import BaseObj
+from playerstats import player_stats
+from settings import *
+from spacecraft import craft
 
 
 class Meteor(BaseObj):
@@ -17,13 +22,24 @@ class Meteor(BaseObj):
         self.color = color
         self.display_surface = DISPLAYSURF
 
+    def destruct(self):
+        #lunch exploded meteor Animation
+        for i in range(randint(7,15)):
+            exploded_meteors_list.add(
+                ExplodedMeteor(self.x,self.y)
+            )
+        # remove meteor form list and delete object
+        meteors.remove_item(self)
+        del self
+
     def move(self,**kwargs):
         if 'vector' in kwargs:
             self.x += kwargs['vector'][0]
             self.y += kwargs['vector'][1]
-        ## TODO:
-        # check if meteor left the game space
-        # if yes remove object and decrease palyer points
+        if left_game_area(self):
+            meteors.remove_item(self)
+            player_stats.points -= 1
+            del self
 
     def draw(self, **kwargs):
         pygame.draw.rect(
@@ -33,16 +49,21 @@ class Meteor(BaseObj):
         )
 
     def colision(self, **kwargs):
+        # check for colision with player's spaceship
+        if detect_colision(self, craft):
+            player_stats.health -= 10
+            self.destruct()
+            return
+        # check for colision with shots
         for i in kwargs['colision_items'].shots:
             if detect_colision(self, i) != None:
                 player_stats.set(2, 'amunition')
                 player_stats.set(1, 'points')
                 #remove bullet from lunched_shots
                 kwargs['colision_items'].remove_item(i)
-                # remove meteor form list and delete object
-                meteors.remove_item(self)
-                del self
-                break
+                self.destruct()
+                return
+
 
 
 class Meteors(object):
@@ -65,13 +86,58 @@ class Meteors(object):
         self.meteors_list.remove(meteor)
 
 
+class ExplodedMeteor(BaseObj):
+    def __init__(self,x,y):
+        super().__init__(x=x,y=y,wx=2,wy=2)
+        self.color = colors['gray']
+        self.display_surface = DISPLAYSURF
+        #generate vector
+        vector_choice = [i for i in range(-5,5)]
+        vector_choice.remove(0)
+        self.vector = [choice(
+            vector_choice
+        ) for i in range(2)]
+
+    def move(self):
+        self.x += self.vector[0]
+        self.y += self.vector[1]
+        if left_game_area(self):
+            exploded_meteors_list.remove_item(self)
+            del self
+
+    def draw(self):
+        pygame.draw.rect(
+            self.display_surface,
+            (255,255,255),
+            (self.x, self.y, self.wx, self.wy)
+        )
+        self.move()
+
+
+class ExplodedMeteorsList(object):
+    def __init__(self):
+        self.list = []
+
+    def add(self,item):
+        self.list.append(item)
+
+    def draw(self):
+        if len(self.list) > 0:
+            for i in self.list:
+                i.draw()
+    def remove_item(self, item):
+        self.list.remove(item)
+
 def add_meteor_row():
     # add first in the row
-    add_meteor(x=grid_x, y=0)
+    x = randint(meteor_x*2, (meteor_x + grid_x)*3)
+    add_meteor(x, y=0)
     # add rest meteors
     root_meteor_x = meteors.meteors_list[-1].x
-    while root_meteor_x + meteor_x + grid_x <= game_area[0]:
-        add_meteor(x=root_meteor_x + meteor_x + grid_x, y=0)
+    while root_meteor_x + x - 25 <= game_area[0]:
+        # grid_size = meteor_x + grid_x
+        add_meteor(x=root_meteor_x + x, y=0)
+        x = randint(meteor_x*2, (meteor_x + grid_x)*3)
         root_meteor_x = meteors.meteors_list[-1].x
 
 def add_meteor(x,y):
@@ -81,5 +147,6 @@ def add_meteor(x,y):
             )
         )
 
-global meteors
+global meteors, exploded_meteors_list
 meteors = Meteors()
+exploded_meteors_list = ExplodedMeteorsList()
